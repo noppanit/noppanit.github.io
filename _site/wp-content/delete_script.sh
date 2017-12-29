27b54da73ecf264650002fdb92287411fc576322
Noppanit Charassinvichai <noppanit.c@gmail.com>
2017-12-28 22:45:21 -0500

    Delete _site folder and ignore it

--- _site/wp-content/delete_script.sh
+++
@@ -1,8 +0,0 @@
-find ./ -type f | while read line; do
-	base_filename=$(basename "$line")
-	found=$(grep -r "$base_filename" ../_posts)
-	if [ -z "$found" ]; then
-		sudo mv "$line" ~/Desktop/temp
-	fi
-
-done
